3a9069e4b2d5c88e41530a1e9d1083b8d5604664
lc <704041774@qq.com>
2019-05-04 15:59:24 +0800

    修改 AddTwoNumbers

--- src/经典148/AddTwoNumbers.java
+++ src/经典148/AddTwoNumbers.java
@@ -2,16 +2,17 @@ package 经典148;
 
 import java.util.List;
 
-class ListNode {
-    int val;
-    ListNode next;
 
-    ListNode(int x) {
-        val = x;
-    }
-}
 
 public class AddTwoNumbers {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         int count = 0;
         ListNode dummy = new ListNode(-1);
@@ -44,13 +45,5 @@ public class AddTwoNumbers {
         return dummy.next;
     }
 
-    public static void main(String[] args) {
-        ListNode node1 = new ListNode(0);
-        ListNode node2 = new ListNode(7);
-        ListNode node3 = new ListNode(3);
-        ListNode node4 = new ListNode(0);
-        node2.next = node3;
-        AddTwoNumbers a = new AddTwoNumbers();
-        a.addTwoNumbers(node1, node2);
-    }
+
 }
